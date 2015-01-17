da92ccf4ad221820d45bbe467fdb705a8ae1ffd8
gtt116 <gtt116@gmail.com>
2015-01-17 13:21:49 +0800

    Ship in gunicorn server
    gunicorn is green wsgi server which support pre-fork mode eventlet server.
    I don't going to re-wheel, so bring in it.
    
    Change-Id: Ied4ecd36dc712499a41991fc331422c6bfbfe9e1

--- sentry/api/__init__.py
+++ sentry/api/__init__.py
@@ -16,6 +16,12 @@ api_configs = [
     cfg.BoolOpt("api_debug",
                 default=False,
                 help="Whether enable builtin api debug mode."),
+    cfg.IntOpt("workers",
+               default=1,
+               help="The number of forked works"),
+    cfg.IntOpt("threads",
+               default=10,
+               help="The number of eventlet threads"),
     cfg.IntOpt("default_items",
                default=20,
                help="Default item return in a API call."),
@@ -27,7 +33,23 @@ CONF.register_group(api_group)
 CONF.register_opts(api_configs, api_group)
 
 
+from gunicorn import glogging
+
+
 def run():
+
+    class Logging(glogging.Logger):
+        """
+        Some hack to override gunicorn's logging
+        """
+        def __init__(self, cfg):
+            self.cfg = cfg
+            self.error_log = LOG
+            self.access_log = LOG
+
+        def setup(self, cfg):
+            pass
+
     from sentry.api import bottle
     from sentry.api.v1.app import app as v1app
 
@@ -37,12 +59,17 @@ def run():
     #root_app.mount('/v2', v2.app)
 
     CONF.log_opt_values(LOG, ori_logging.DEBUG)
-    LOG.info("Sentry api start running.")
+    LOG.info("Sentry API start running.")
 
     for route in root_app.routes:
         LOG.debug(str(route))
 
     bottle.run(root_app,
+               server='gunicorn',
+               worker_class='eventlet',
+               logger_class=Logging,
+               workers=CONF.api.workers,
+               threads=CONF.api.threads,
                debug=CONF.api.api_debug,
                port=CONF.api.listen_port,
                host=CONF.api.listen_host)
