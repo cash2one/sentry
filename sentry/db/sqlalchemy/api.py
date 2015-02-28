c1f7be3d20bbe806a0ab7bc58e381e39b996f1df
gtt116 <gtt116@gmail.com>
2015-02-28 09:47:28 +0800

    Using SQLalchemy autocommit mode
    Also add some config for sqlalchemy create_engine
    
    Change-Id: I00881833f1a72e8663435e5dcd43e203c293e2e6

--- sentry/db/sqlalchemy/api.py
+++ sentry/db/sqlalchemy/api.py
@@ -15,35 +15,34 @@ def get_backend():
 def event_create(event):
     se = session.get_session()
 
-    # dump to json
-    raw_json = jsonutils.dumps(event.raw_json)
-    raw_message = models.RawMessage(json=raw_json)
-    se.add(raw_message)
+    with se.begin():
+        raw_json = jsonutils.dumps(event.raw_json)
+        raw_message = models.RawMessage(json=raw_json)
+        se.add(raw_message)
 
-    event = models.Event(
-        message_id=event.message_id,
-        token=event.token,
-        object_id=event.object_id,
-        raw_message=raw_message,
-        is_admin=event.is_admin,
-        request_id=event.request_id,
-        roles=jsonutils.dumps(event.roles),  # json
-        project_id=event.project_id,
-        project_name=event.project_name,
-        user_name=event.user_name,
-        user_id=event.user_id,
-        event_type=event.event_type,
-        payload=jsonutils.dumps(event.payload),  # json
-        level=event.level,
-        publisher_id=event.publisher_id,
-        remote_address=event.remote_address,
-        timestamp=event.timestamp,
-        service=event.service,
-        binary=event.binary,
-        hostname=event.hostname,
-    )
-    se.add(event)
-    se.commit()
+        event = models.Event(
+            message_id=event.message_id,
+            token=event.token,
+            object_id=event.object_id,
+            raw_message=raw_message,
+            is_admin=event.is_admin,
+            request_id=event.request_id,
+            roles=jsonutils.dumps(event.roles),  # json
+            project_id=event.project_id,
+            project_name=event.project_name,
+            user_name=event.user_name,
+            user_id=event.user_id,
+            event_type=event.event_type,
+            payload=jsonutils.dumps(event.payload),  # json
+            level=event.level,
+            publisher_id=event.publisher_id,
+            remote_address=event.remote_address,
+            timestamp=event.timestamp,
+            service=event.service,
+            binary=event.binary,
+            hostname=event.hostname,
+        )
+        se.add(event)
 
 
 def _validate_search_dict(model, search_dict):
--- sentry/db/sqlalchemy/session.py
+++ sentry/db/sqlalchemy/session.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from oslo.config import cfg
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -16,7 +17,13 @@ database_opts = [
                "database",
                secret=False),
     cfg.BoolOpt("sql_debug", default=False,
-                help="Whether echo raw sql in log.")
+                help="Whether echo raw sql in log."),
+    cfg.IntOpt("sql_max_overflow", default=10,
+               help='The SQLAlchemy max_overflow config'),
+    cfg.IntOpt("sql_pool_size", default=5,
+               help='The SQLAlchemy pool_size config'),
+    cfg.IntOpt("sql_pool_recycle", default=3600,
+               help='The SQLAlchemy pool_recycle config'),
 
 ]
 CONF = cfg.CONF
@@ -33,9 +40,16 @@ def get_engine():
         if CONF.sql_debug:
             logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-        kwargs = dict(
-            echo=CONF.sql_debug,
-        )
+        connection_dict = sqlalchemy.engine.url.make_url(CONF.sql_connection)
+
+        if "sqlite" not in connection_dict.drivername:
+            kwargs = dict(
+                max_overflow=CONF.sql_max_overflow,
+                pool_size=CONF.sql_pool_size,
+                pool_recycle=CONF.sql_pool_recycle,
+            )
+        else:
+            kwargs = dict()
 
         ENGINE = create_engine(CONF.sql_connection, **kwargs)
     return ENGINE
@@ -57,6 +71,6 @@ def get_session():
     global MAKER
     if not MAKER:
         engine = get_engine()
-        MAKER = sessionmaker(bind=engine)
+        MAKER = sessionmaker(bind=engine, autocommit=True)
 
     return MAKER()
